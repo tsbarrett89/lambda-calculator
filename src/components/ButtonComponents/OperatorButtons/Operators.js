f3c2c7a2d7fca756b193b367bb25545baa424441
Thomas Barrett <tsbarrett89@gmail.com>
2019-10-08 20:10:38 -0600

    finished MVP

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import {operators} from '../../../data';
+import OperatorButton from './OperatorButton';
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
+const Operator = (props) => {
   // STEP 2 - add the imported data to state
+  const [buttonOperator, setButtonOperator] = useState(operators)
+
   return (
     <div>
+      {buttonOperator.map((operator, index) => {
+        return < OperatorButton key={index}  text={operator.char} />
+      })}
+
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
     </div>
   );
 };
+
+export default Operator
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
   return (
-    <>
+    <button className="specialBtns" onClick={() => console.log(props.text)}>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.text}
+    </button>
   );
 };
+
+export default SpecialButton
